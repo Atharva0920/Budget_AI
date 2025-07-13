d7374b5d3021c98f697b5019dc76113e8f308714
Atharva Ganmote <aganmote@ptc.com>
2025-07-13 13:22:56 +0530

    Project - Inintial setup
    - Project repo setup with initial files
    - Initial commit with basic React app structure

---
+++ bachat-raah-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Bachat-Raah">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
