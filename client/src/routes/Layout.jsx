f31322ff73986830e1c4994b7449081973463a58
Atharva Ganmote <aganmote@ptc.com>
2025-07-25 23:49:09 +0530

    Fix: #15 - Sidebar auto close on nav-itm selection
    - Implemented sidebar auto-close functionality
        when a navigation item is selected
    - updated color of Menu in dark mode for better
        visibility

--- client/src/routes/Layout.jsx
+++ client/src/routes/Layout.jsx
@@ -15,7 +15,7 @@ export default function Layout() {
       <aside className={`fixed top-0 left-0 h-full w-64 z-40 bg-white transform transition-transform duration-300 ease-in-out
                 ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} 
                 lg:translate-x-0`}>
-        <Home closeMenu={() => setIsMenuOpen(false)} setTitle />
+        <Home closeMenu={() => setIsMenuOpen(false)} setTitle isMenuOpen onMenuClick={() => setIsMenuOpen(!isMenuOpen)}/>
       </aside>
 
       {isMenuOpen && (
