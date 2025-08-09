640a7f1558072927990d32a5e9f652c1a585c9a7
Atharva Ganmote <aganmote@ptc.com>
2025-08-09 23:26:13 +0530

    Setup backend
    - Setup spring backend

---
+++ budget-backend/src/test/java/com/aganmote/budget_backend/BudgetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aganmote.budget_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
